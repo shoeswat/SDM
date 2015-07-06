ceebb94cd9d057aec3c248e6fb03f469dc95ee60
shoeswat <yugarshi.mondal@gmail.com>
2015-07-06 16:48:58 -0700

    model fits

--- server.R
+++ server.R
@@ -1,6 +1,17 @@
 library(shiny)
+#requireNamespace('rgeos')
+require(rgeos)
+require(plyr)
+require(ggplot2)
+require(raster)
+require(maptools)
+require(grid)
 library(RCurl)
 source('SDM_library.R')
+
+current_vars <- load.predictors(1)
+holocene_vars <-load.predictors(2)
+lgm_vars<-load.predictors(3)
 
 #Tsuga heterophylla
 
@@ -9,10 +20,14 @@ shinyServer(function(input, output){
 	dummy <- reactive({
 		coords <- read.csv(text = getURL(paste0("https://ecoengine.berkeley.edu/api/observations/?q=scientific_name:%22",URLencode(input$species),"%22&fields=geojson&page_size=300&georeferenced=True&format=csv")))
 		coords <- coords[,1:2]
+
+		#prep inputs/outputs; build model
+		data.ready <- prep.species(coords[,1:2], current_vars, nb.absences=10000)
+		model <- gbm.step(data.ready, 1:19, 'pres', tree.complexity=3, learning.rate=0.05, max.trees=100000000, bag.fraction=0.75)
 	})
 
-	output$view <- renderTable({
-		head(dummy())
+	output$view <- renderPrint({
+		summary(dummy())
 	})
 
 })
