7d06e7016eb7b722490acd03d84f7d97c1df35d6
shoeswat <yugarshi.mondal@gmail.com>
2015-07-06 16:30:44 -0700

    add reactive coordinate loading

--- server.R
+++ server.R
@@ -6,12 +6,14 @@ source('SDM_library.R')
 
 shinyServer(function(input, output){
 	
-	output$view <- renderTable({
+	dummy <- data.frame()
+
+	dummy <- reactive({
 		coords <- read.csv(text = getURL(paste0("https://ecoengine.berkeley.edu/api/observations/?q=scientific_name:%22",URLencode(input$species),"%22&fields=geojson&page_size=300&georeferenced=True&format=csv")))
-		coords
-		coords <- coords[,1:2]
-		names(coords) <- c("longitude", "latitude")
-		head(coords)
+	})
+
+	output$view <- renderTable({
+		head(dummy())
 	})
 
 })
