f69f82977dea6cf3321e2bf2ca05fe7adc2a0eb0
shoeswat <yugarshi.mondal@gmail.com>
2015-07-06 16:31:57 -0700

    remove df declaration

--- server.R
+++ server.R
@@ -6,8 +6,6 @@ source('SDM_library.R')
 
 shinyServer(function(input, output){
 	
-	dummy <- data.frame()
-
 	dummy <- reactive({
 		coords <- read.csv(text = getURL(paste0("https://ecoengine.berkeley.edu/api/observations/?q=scientific_name:%22",URLencode(input$species),"%22&fields=geojson&page_size=300&georeferenced=True&format=csv")))
 	})
