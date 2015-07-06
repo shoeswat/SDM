04465391aa22b1d08d846524ff87994df93ea8d8
shoeswat <yugarshi.mondal@gmail.com>
2015-07-06 16:35:25 -0700

    change size of array: coords

--- server.R
+++ server.R
@@ -8,6 +8,7 @@ shinyServer(function(input, output){
 	
 	dummy <- reactive({
 		coords <- read.csv(text = getURL(paste0("https://ecoengine.berkeley.edu/api/observations/?q=scientific_name:%22",URLencode(input$species),"%22&fields=geojson&page_size=300&georeferenced=True&format=csv")))
+		coords <- coords[,1:2]
 	})
 
 	output$view <- renderTable({
