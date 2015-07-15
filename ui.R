77ed703c276ae41386400a25a5c4be42f7b2db31
shoeswat <yugarshi.mondal@gmail.com>
2015-07-14 18:39:30 -0700

    add lat lon inputs to UI

--- ui.R
+++ ui.R
@@ -36,11 +36,33 @@ shinyUI(fluidPage(
                   "Project using only Upload Data"=3)
 			),
 
+
+			div(id="latNorth_div",textInput(inputId="latNorth", label="Lat Noth", value = 43.0456)),
+			tags$head(tags$style(type="text/css", "#latNorth_div {display: inline-block; text-align: center;}")),
+			tags$head(tags$style(type="text/css", "#latNorth {max-width: 100px}")),
+
+			div(id="latSouth_div",textInput(inputId="latSouth", label="Lat South", value = 42.0345)),
+			tags$head(tags$style(type="text/css", "#latSouth_div {display: inline-block; text-align: center;}")),
+			tags$head(tags$style(type="text/css", "#latSouth {max-width: 100px}")),
+
+			div(id="lonEast_div",textInput(inputId="lonEast", label="Lon East", value = -120.0530)),
+			tags$head(tags$style(type="text/css", "#lonEast_div {display: inline-block; text-align: center;}")),
+			tags$head(tags$style(type="text/css", "#lonEast {max-width: 100px}")),
+
+			div(id="lonWest_div",textInput(inputId="lonWest", label="Lon West", value = -123.0638)),
+			tags$head(tags$style(type="text/css", "#lonWest_div {display: inline-block; text-align: center;}")),
+			tags$head(tags$style(type="text/css", "#lonWest {max-width: 100px}")),
+
+			tags$br(),
+
 			actionButton("project","Project"),
 
 			helpText("Troubleshooting: ", tags$br(), "1. If species can't be found, use ",
 				tags$a(href="https://holos.berkeley.edu/explore/#&q=&page_size=100", "Holos"),
-				"to verify that observation data exists.", tags$br(), "2. Computations will take some time; Status bar appears at top right of page."),
+				"to verify that observation data exists.", tags$br(),
+				"2. If no input range is given, California will be plotted", tags$br(),
+				"3. Computations will take some time; Status bar appears at top right of page."
+			),
 
 			tags$br(),
 
